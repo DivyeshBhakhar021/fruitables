a474031ed9278b2ffc32945d761fc819f2cf2a19
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-03-13 09:26:01 +0530

    page_stuep

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Home from './user/container/Home/Home';
 import Header from './user/component/Header/Header';
 import Footer from './user/component/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Shop from './user/container/Shop/Shop';
+import Shop_detail from './user/container/Shop_detail/Shop_detail';
+import Contact from './user/container/Contact/Contact';
+import Card from './user/container/Page/Card/Card';
+import Chackout from './user/container/Page/Chackout/Chackout';
+import Testimonial from './user/container/Page/Testimonial/Testimonial';
+import Error from './user/container/Error/Error';
 
 function App() {
   return (
     <>
     
     <Header />
-      <Home />
+      <Routes >
+      <Route  exect path='/' element={<Home/>}/>
+      <Route  exect path='/shop' element={<Shop/>}/>
+      <Route  exect path='/shop_detail' element={<Shop_detail />}/>
+      <Route  exect path='/contact' element={<Contact />}/>
+      <Route  exect path='/card' element={<Card />}/>
+      <Route  exect path='/chackout' element={<Chackout />}/>
+      <Route  exect path='/testimonial' element={<Testimonial />}/>
+      <Route  exect path='/error' element={<Error />}/>
+      </Routes>
     <Footer />
     </>
   );
