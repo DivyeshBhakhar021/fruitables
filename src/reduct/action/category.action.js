18d69ff6a7aee643789154cebeb6d657c496d792
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-08-23 11:02:05 +0530

    cd

--- src/reduct/action/category.action.js
+++ src/reduct/action/category.action.js
@@ -1,12 +1,70 @@
+// import { ADD_CATEGORY, DELETE_CATEGORY, GET_CATEGORY, UPDATE_CATEGORY } from '../reducer/action.type';
+
+// export const getCategory = () => async (dispatch) => {
+//     try {
+//         const response = await fetch('http://localhost:5000/api/v1/categories/get-categorie');
+//         const data = await response.json();
+//         console.log(response);
+        
+//         dispatch({ type: GET_CATEGORY, payload: data.data });
+//     } catch (error) {
+//         console.error('Error fetching categories:', error);
+//     }
+// };  
+
+// export const addCategory = (category) => async (dispatch) => {
+//     try {
+//         const response = await fetch('http://localhost:5000/api/v1/categories/addcategories/', {
+//             method: 'POST',
+//             body: JSON.stringify(category),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         });
+//         const data = await response.json();
+//         dispatch({ type: ADD_CATEGORY, payload: data });
+//     } catch (error) {
+//         console.error('Error adding category:', error);
+//     }
+// };
+
+// export const deleteCategory = (id) => async (dispatch) => {
+//     try {
+//         await fetch(`http://localhost:5000/api/v1/categories/deleteCategories/${id}`, {
+//             method: 'DELETE',
+//         });
+//         dispatch({ type: DELETE_CATEGORY, payload: id });
+//     } catch (error) {
+//         console.error('Error deleting category:', error);
+//     }
+// };
+
+// export const updateCategory = (category) => async (dispatch) => {
+//     console.log("category",category._id);
+    
+//     try {
+//      const response =  await fetch(`http://localhost:5000/api/v1/categories/updateCategories/${category._id}`, {
+//             method: 'PUT',
+//             body: JSON.stringify(category),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         });
+//         const data = await response.json();
+//         dispatch({ type: UPDATE_CATEGORY, payload: data });
+//     } catch (error) {
+//         console.error('Error updating category:', error);
+//     }
+// };
+
+// import axios from 'axios';
+import axiosInstance from '../../utilites/axiosInstance';
 import { ADD_CATEGORY, DELETE_CATEGORY, GET_CATEGORY, UPDATE_CATEGORY } from '../reducer/action.type';
 
 export const getCategory = () => async (dispatch) => {
     try {
-        const response = await fetch('http://localhost:5000/api/v1/categories/get-categorie');
-        const data = await response.json();
-        console.log(response);
-        
-        dispatch({ type: GET_CATEGORY, payload: data.data });
+        const response = await axiosInstance.get('categories/get-categorie');
+        dispatch({ type: GET_CATEGORY, payload: response.data.data });
     } catch (error) {
         console.error('Error fetching categories:', error);
     }
@@ -14,25 +72,22 @@ export const getCategory = () => async (dispatch) => {
 
 export const addCategory = (category) => async (dispatch) => {
     try {
-        const response = await fetch('http://localhost:5000/api/v1/categories/addcategories/', {
-            method: 'POST',
-            body: JSON.stringify(category),
+        const response = await axiosInstance.post('categories/addcategories/', category, {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
-        const data = await response.json();
-        dispatch({ type: ADD_CATEGORY, payload: data });
+        dispatch({ type: ADD_CATEGORY, payload: response.data });
     } catch (error) {
         console.error('Error adding category:', error);
     }
 };
 
 export const deleteCategory = (id) => async (dispatch) => {
+    console.log(id);
+    
     try {
-        await fetch(`http://localhost:5000/api/v1/categories/deleteCategories/${id}`, {
-            method: 'DELETE',
-        });
+        await axiosInstance.delete(`categories/deleteCategories/${id}`);
         dispatch({ type: DELETE_CATEGORY, payload: id });
     } catch (error) {
         console.error('Error deleting category:', error);
@@ -40,19 +95,17 @@ export const deleteCategory = (id) => async (dispatch) => {
 };
 
 export const updateCategory = (category) => async (dispatch) => {
-    console.log("category",category._id);
+    console.log("category", category._id);
     
     try {
-     const response =  await fetch(`http://localhost:5000/api/v1/categories/updateCategories/${category._id}`, {
-            method: 'PUT',
-            body: JSON.stringify(category),
+        const response = await axiosInstance.put(`categories/updateCategories/${category._id}`, category, {
             headers: {
                 'Content-Type': 'application/json',
             },
         });
-        const data = await response.json();
-        dispatch({ type: UPDATE_CATEGORY, payload: data });
+        dispatch({ type: UPDATE_CATEGORY, payload: response.data });
     } catch (error) {
-        console.error('Error updating category:', error);
+        console.log(error);
     }
 };
+
--- src/reduct/reducer/category.reducer.js
+++ src/reduct/reducer/category.reducer.js
@@ -1,3 +1,52 @@
+// import { ADD_CATEGORY, DELETE_CATEGORY, GET_CATEGORY, UPDATE_CATEGORY } from "./action.type";
+
+// const initialState = {
+//     isLoading: false,
+//     category: [],
+//     error: null
+// };
+
+// export const categoryReducer = (state = initialState, action) => {
+//     console.log(action);
+
+//     switch (action.type) {
+//         case GET_CATEGORY:
+//             return {
+//                 ...state,
+//                 isLoading: false,
+//                 category: action.payload,
+//                 error: null
+//             };
+
+//         case ADD_CATEGORY:
+//             return {
+//                 ...state,
+//                 isLoading: false,
+//                 category: state.category.concat(action.payload.data),
+//                 error: null
+//             };
+
+//         case DELETE_CATEGORY:
+//             return {
+//                 ...state,
+//                 isLoading: false,
+//                 category: state.category.filter((v) => v._id !== action.payload),
+//                 error: null
+//             };
+
+//         case UPDATE_CATEGORY:
+//             return {
+//                 ...state,
+//                 isLoading: false,
+//                 category: state.category.map((v) => v._id === action.payload.data._id ? action.payload.data : v),
+//                 error: null
+//             };
+
+//         default:
+//             return state;
+//     }
+// };
+
 import { ADD_CATEGORY, DELETE_CATEGORY, GET_CATEGORY, UPDATE_CATEGORY } from "./action.type";
 
 const initialState = {
@@ -22,7 +71,7 @@ export const categoryReducer = (state = initialState, action) => {
             return {
                 ...state,
                 isLoading: false,
-                category: state.category.concat(action.payload.data),
+                category: state.category.concat(action.payload.data), // `payload.data` is expected from axios
                 error: null
             };
 
